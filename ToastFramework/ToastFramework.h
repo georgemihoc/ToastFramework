9393fdc3e8032cc4234b89498bd32d66fda35009
George Mihoc <george.mihoc@gmail.com>
2022-03-14 01:47:39 +0200

    Initial Commit

---
+++ ToastFramework/ToastFramework.h
@@ -0,0 +1,18 @@
+//
+//  ToastFramework.h
+//  ToastFramework
+//
+//  Created by George Mihoc on 14.03.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ToastFramework.
+FOUNDATION_EXPORT double ToastFrameworkVersionNumber;
+
+//! Project version string for ToastFramework.
+FOUNDATION_EXPORT const unsigned char ToastFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ToastFramework/PublicHeader.h>
+
+
