469fe817cfc031ce6038829cdbb028a20ee1b633
George Mihoc <george.mihoc@gmail.com>
2022-05-19 01:52:02 +0300

    [main] tweak example accordingly + refactoring

--- ToastFrameworkExample/ToastFrameworkExample/ViewController.swift
+++ ToastFrameworkExample/ToastFrameworkExample/ViewController.swift
@@ -5,21 +5,53 @@
 //  Created by George Mihoc on 19.05.2022.
 //
 
-import UIKit
 import ToastFrameworkSwift
+import UIKit
 
 class ViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Setup configurable properties
+        setupToastManager()
     }
-    
+
     @IBAction func basicToastSelected(_ sender: UIButton) {
+        presentBasicToast()
+    }
+
+    @IBAction func basicToastGraySelected(_ sender: UIButton) {
+        presentBasicGrayToast()
+    }
+
+    @IBAction func ctaToastSelected(_ sender: UIButton) {
+        presentCTAToast()
+    }
+}
+
+// MARK: - Private
+
+private extension ViewController {
+    /// Setup configurable properties
+    func setupToastManager() {
+        ToastManager.shared.isTapToDismissEnabled = true
+        ToastManager.shared.isSwipeToDismissEnabled = true
+        ToastManager.shared.isQueueEnabled = true
+        ToastManager.shared.duration = 2
+        ToastManager.shared.areHapticsEnabled = true
+    }
+
+    func presentBasicToast() {
         view.showToast(message: "This is a Basic Toast.")
     }
-    
-    @IBAction func basicToastGraySelected(_ sender: UIButton) {
+
+    func presentBasicGrayToast() {
         view.showToast(message: "This is a Basic Gray Toast.", color: .gray)
     }
-    
+
+    func presentCTAToast() {
+        view.showCTAToast(message: "This is a CTA Toast.", actionTitle: "Press here") { [weak self] in
+            self?.view.showToast(message: "This is the follow-up Toast.", color: .systemGreen)
+        }
+    }
 }
